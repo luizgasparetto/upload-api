6998b1a258dcdb390ccd00e269e4a5cd10e4b01b
luizgasp <luizegasp@gmail.com>
2022-04-18 15:00:27 -0300

    fixed

--- src/modules/objects/repositories/IObjectsRepository.ts
+++ src/modules/objects/repositories/IObjectsRepository.ts
@@ -5,7 +5,7 @@ import { ObjectEntity } from "../entities/ObjectEntity";
 
 interface IObjectRepository {
   create({ width, height, user_id }: ICreateObjectDTO): Promise<void>;
-  updateImage({ object_id, image_url }: IUpdateObjectDTO): Promise<void>;
+  updateImage({ object_id, url }: IUpdateObjectDTO): Promise<void>;
   delete({ object_id }: IDeleteObjectDTO): Promise<void>;
   getObjects(): Promise<ObjectEntity[]>;
   findById(id: string): Promise<ObjectEntity>
